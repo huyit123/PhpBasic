1a39c3210defab67165f5a4241cf24b0ac5f83c9
huyit123 <quanghuy060392@gmail.com>
2024-09-09 20:40:21 +0700

    first commit

---
+++ section-01.php
@@ -0,0 +1,27 @@
+<?php 
+$ten = "John Doe";
+$tuoi = 25;
+$number = 123;
+
+$firstName = 'Nguyễn';
+$lastName = 'Châu';
+
+$a = '';
+$b = '';
+
+$total = $a + $b; 
+
+?>
+
+<html>
+<body>
+  <h1>Chào mừng bạn đến với trang web của tôi</h1>
+  <p>Hôm nay là: <?php echo $total; ?></p>
+<p>
+
+</p>
+  
+</body>
+</html>
+
+
